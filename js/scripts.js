12d33fcb9aedd96a4b8443e03573359d85a82f78
Alexander <sitesmake@gmail.com>
2018-09-10 21:14:13 +0300

    Add js to map

--- js/scripts.js
+++ js/scripts.js
@@ -56,3 +56,29 @@ window.addEventListener("keydown", function (evt) {
     }
   }
 });
+
+
+// map
+var mapLink = document.querySelector(".contacts-button-map");
+
+var mapPopup = document.querySelector(".popup-map");
+var mapClose = mapPopup.querySelector(".close-button");
+
+mapLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.add("modal-show");
+});
+
+mapClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (mapPopup.classList.contains("modal-show")) {
+      mapPopup.classList.remove("modal-show");
+    }
+  }
+});
